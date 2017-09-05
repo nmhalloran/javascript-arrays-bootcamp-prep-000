7c5efaad9714a35a608fce265192e06a54015eef
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-05 20:17:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,18 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+  return array
+}
